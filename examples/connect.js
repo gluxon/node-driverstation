51e57a0b46f2a37b46c121bcd0d0f315a1f96ec2
gluxon <bcheng.gt@gmail.com>
2013-06-01 22:34:40 -0400

    Changed API. Added connection timeout

--- examples/connect.js
+++ examples/connect.js
@@ -1,9 +1,10 @@
-var driverstation = new (require('..'));
+var driverstation = (require('..'));
 
 var options = {
 	ip: '10.1.78.2'
 };
 
-driverstation.connect(options, function(err) {
+driverstation.start(options);
+driverstation.connect(function(err) {
 	if (err) throw err;
 });
