998f527199a238bc88e8b8fd2a7832717b26d193
gluxon <bcheng.gt@gmail.com>
2013-06-01 23:08:11 -0400

    Fixed enable and estop. Added option for team number

--- examples/connect.js
+++ examples/connect.js
@@ -1,10 +1,13 @@
 var driverstation = (require('..'));
 
 var options = {
-	ip: '10.1.78.2'
+	ip: '10.1.78.2',
+	teamID: 178
 };
 
 driverstation.start(options);
 driverstation.connect(function(err) {
 	if (err) throw err;
 });
+
+driverstation.enable();
