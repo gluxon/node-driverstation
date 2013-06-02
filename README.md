ac522cb87c7f9468576fee71bddb1b9887053290
gluxon <bcheng.gt@gmail.com>
2013-06-02 13:24:16 -0400

    emit events for connect/disconnect. utility function for team number -> ip

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Node.js API for the client-side FRC Driver Station
 ## License
 
 node-driverstation is written under the [MIT License](http://opensource.org/licenses/MIT)
+
+### 0.2
+- Emit connect and disconnect
+- Utility function for converting team number to IP
+
+### 0.1
+- Initial Release
--- examples/connect.js
+++ examples/connect.js
@@ -1,13 +1,17 @@
 var driverstation = (require('..'));
 
 var options = {
-	ip: '10.1.78.2',
 	teamID: 178
 };
 
 driverstation.start(options);
-driverstation.connect(function(err) {
-	if (err) throw err;
+
+driverstation.connection.on('connect', function() {
+	console.log("connected!");
+});
+
+driverstation.connection.on('disconnect', function() {
+	console.log("disconnected");
 });
 
 driverstation.enable();
